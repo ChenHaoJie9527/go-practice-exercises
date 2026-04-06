e71cf4a73d8c410ba44fb3a59aec185e6ab86980
momotom <a18520145493@gmail.com>
2026-04-06 16:44:12 +0800

    feat: 更新range

--- range/index.go
+++ range/index.go
@@ -10,17 +10,20 @@ func main() {
 	}
 	fmt.Println(sum)
 
+	//range 在数组和 slice 中提供对每项的索引和值的访问。 上面我们不需要索引，所以我们使用 空白标识符 _ 将其忽略。 实际上，我们有时候是需要这个索引的。
 	for i, num := range nums {
 		if num == 2 {
 			fmt.Println("i", i)
 		}
 	}
 
+	//range 在 map 中迭代键值对
 	kvs := map[string]string{"a": "apple", "b": "banana"}
 	for k, v := range kvs {
 		fmt.Printf("%s -> %s\n", k, v)
 	}
 
+	//range 也可以只遍历 map 的键。
 	for s := range kvs {
 		fmt.Println("key:", s) // 打印 map的key
 	}
